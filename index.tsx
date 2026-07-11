3a38c7472b252969ce3c2242a382cb99eb933e04
Furia <furiarocktshirtscolombia@gmail.com>
2026-07-11 15:26:09 -0500

    Render DesignerApp conditionally based on URL hash

--- index.tsx
+++ index.tsx
@@ -1,16 +1,13 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import DesignerApp from './src/designer/components/DesignerApp';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
+    throw new Error("Could not find root element to mount to");
 }
 
 const root = ReactDOM.createRoot(rootElement);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const isDesigner = window.location.hash.startsWith('#/disenador');
+root.render(isDesigner ? <DesignerApp /> : <App />);
