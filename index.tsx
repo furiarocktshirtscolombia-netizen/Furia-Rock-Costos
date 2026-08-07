d59dccdc620dba63105379eb5cb58dddb097171e
Furia <furiarocktshirtscolombia@gmail.com>
2026-08-07 18:41:52 -0500

    Import compiled index.css in entry point

--- index.tsx
+++ index.tsx
@@ -1,3 +1,4 @@
+import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
@@ -5,7 +6,7 @@ import DesignerApp from './src/designer/components/DesignerApp';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
-    throw new Error("Could not find root element to mount to");
+      throw new Error("Could not find root element to mount to");
 }
 
 const root = ReactDOM.createRoot(rootElement);
