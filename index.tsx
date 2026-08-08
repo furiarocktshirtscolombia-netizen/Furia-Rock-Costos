d3d6e718ceee2b834801132222cfc1ffe94f7203
Furia <furiarocktshirtscolombia@gmail.com>
2026-08-07 19:18:07 -0500

    Remove DesignerApp and simplify rendering
    Removed DesignerApp module and updated rendering logic to always render App.

--- index.tsx
+++ index.tsx
@@ -2,13 +2,15 @@ import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import DesignerApp from './src/designer/components/DesignerApp';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
       throw new Error("Could not find root element to mount to");
 }
 
+// El antiguo modulo Disenador fue eliminado. Si alguien entra directamente a
+// #/disenador, lo redirigimos de forma segura al Cotizador (pantalla principal).
+if (window.location.hash.startsWith('#/disenador')) { window.location.hash = ''; }
+
 const root = ReactDOM.createRoot(rootElement);
-const isDesigner = window.location.hash.startsWith('#/disenador');
-root.render(isDesigner ? <DesignerApp /> : <App />);
+root.render(<App />);
